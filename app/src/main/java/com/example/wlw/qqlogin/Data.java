7fa97da45e30f82ae8a7b524267d5bc9f34bf292
lq2014 <lq2014@qq.com>
2016-11-10 22:50:31 +0800

    凌秋作业

---
+++ app/src/main/java/com/example/wlw/qqlogin/Data.java
@@ -0,0 +1,12 @@
+package com.example.wlw.qqlogin;
+
+/**
+ * Created by Administrator on 2016/10/30.
+ */
+public final class Data {
+    public static final String[][] USER={
+            {"101","111","0"},
+            {"666","111","1"},
+            {"1003","111","2"}
+    };
+}
